4f8f53e211e464626309bd5292b6d62cb2d74f3d
Abdi7309 <512875@vistacollege.nl>
2023-03-30 15:40:00 +0200

    uitloggen knop

--- menu.php
+++ menu.php
@@ -2,5 +2,5 @@
     <div id="white-box" onclick="window.location.href='./home.php'" ><h1>Startpagina</h1><img id="foto" src="home_house_icon-icons.com_49851.png" alt=""></div>
     <div id="white-box2" onclick="window.location.href='./Zoeken.php'"><h1>Zoeken</h1><img id="foto" src="magnifier-1_icon-icons.com_56924.png" alt=""></div>
     <div id="white-box3" onclick="window.location.href='./Profiel.php'"><h1>Profiel</h1><img id="foto" src="3289576-individual-man-people-person_107097.png" alt=""></div>
-    <div id="white-box4" onclick="window.location.href='./logout.php'"><h1>Uitloggen</h1><img id="foto1" src="power-button-on-off-switch-sign-for-pictogram-art-illustration-logo-apps-website-or-graphic-design-element-format-png.webp" alt=""></div>
+    <div id="white-box4" onclick="window.location.href='./logout.php'"><img id="foto1" src="power-button-on-off-switch-sign-for-pictogram-art-illustration-logo-apps-website-or-graphic-design-element-format-png.webp" alt=""><h1>Uitloggen</h1></div>
 </div>
